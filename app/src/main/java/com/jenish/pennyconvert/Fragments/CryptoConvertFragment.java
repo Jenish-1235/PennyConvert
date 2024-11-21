d01a35a91faffaaf219b96884348a65ee325a76d
Jenish Togadiya <jenishtogadiya549@gmail.com>
2024-11-21 17:38:04 +0530

    feature-gemini-fact-generation-for-crypto

--- app/src/main/java/com/jenish/pennyconvert/Fragments/CryptoConvertFragment.java
+++ app/src/main/java/com/jenish/pennyconvert/Fragments/CryptoConvertFragment.java
@@ -4,9 +4,11 @@ import android.os.Bundle;
 
 import androidx.fragment.app.Fragment;
 
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
@@ -14,16 +16,25 @@ import android.widget.LinearLayout;
 import android.widget.Spinner;
 import android.widget.TextView;
 
+import com.google.ai.client.generativeai.GenerativeModel;
+import com.google.ai.client.generativeai.java.GenerativeModelFutures;
+import com.google.ai.client.generativeai.type.Content;
+import com.google.ai.client.generativeai.type.GenerateContentResponse;
+import com.google.common.util.concurrent.FutureCallback;
+import com.google.common.util.concurrent.Futures;
+import com.google.common.util.concurrent.ListenableFuture;
 import com.jenish.pennyconvert.R;
 import com.jenish.pennyconvert.models.CryptoModel;
 import com.jenish.pennyconvert.services.CoinLayerApiService;
+import com.jenish.pennyconvert.services.GeminiAiApi;
 
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
 import java.util.concurrent.Executor;
+import java.util.concurrent.Executors;
 
-public class CryptoConvertFragment extends Fragment {
+public class CryptoConvertFragment extends Fragment{
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
@@ -67,7 +78,6 @@ public class CryptoConvertFragment extends Fragment {
 
                             baseCryptoInput.setAdapter(new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, cryptoList));
                             targetCryptoInput.setAdapter(new ArrayAdapter<String>(getActivity(), android.R.layout.simple_list_item_1, cryptoList));
-
                             convertButton.setOnClickListener(new View.OnClickListener() {
                                 @Override
                                 public void onClick(View v) {
@@ -86,10 +96,10 @@ public class CryptoConvertFragment extends Fragment {
                                     factsViewLinearLayout.setVisibility(View.VISIBLE);
                                     convertValueView.setVisibility(View.VISIBLE);
 
-
+                                    getFacts(baseCrypto + " Crypto Currency", 1, factView1, factView2);
+                                    getFacts(targetCrypto + " Crypto Currency", 2, factView1, factView2);
                                 }
                             });
-
                         }
                     });
                 }
@@ -97,7 +107,37 @@ public class CryptoConvertFragment extends Fragment {
         });
         cryptoThread.start();
 
-
         return view;
+    }
+    private void getFacts(String currency, int factView, TextView factView1, TextView factView2){
+        GenerativeModel gm = new GenerativeModel("gemini-1.5-flash-8b", "AIzaSyBt5pEBnSGL2k4A-GTO-6M3zY6ta6O6--A");
+        GenerativeModelFutures model = GenerativeModelFutures.from(gm);
+
+        Content content = new Content.Builder().addText("Give me 1 random fact about " + currency).build();
+        Executor executor = Executors.newSingleThreadExecutor();
+        ListenableFuture<GenerateContentResponse> response = model.generateContent(content);
+        Futures.addCallback(
+                response,
+                new FutureCallback<GenerateContentResponse>() {
+                    @Override
+                    public void onSuccess(GenerateContentResponse result) {
+                        String resultText = result.getText();
+                        getActivity().runOnUiThread(new Runnable() {
+                            @Override
+                            public void run() {
+                                if (factView == 1) {
+                                    factView1.setText(resultText);
+                                } else {
+                                    factView2.setText(resultText);
+                                }
+                            }
+                        });
+                    }
+                    @Override
+                    public void onFailure(Throwable t) {
+                        Log.i("Datatata", t.getMessage());
+                    }
+
+                }, executor);
     }
 }
