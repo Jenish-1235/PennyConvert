7a406f3b86768a67a0297d4089f714b0abaa3b50
Jenish Togadiya <jenishtogadiya549@gmail.com>
2024-11-21 13:27:26 +0530

    progress-crypto-model-class-and-ui

---
+++ app/src/main/java/com/jenish/pennyconvert/models/CryptoModel.java
@@ -0,0 +1,12 @@
+package com.jenish.pennyconvert.models;
+
+import java.util.HashMap;
+
+public class CryptoModel {
+    public String success;
+    public String terms;
+    public String privacy;
+    public String timestamp;
+    public String target;
+    public HashMap<String, Double> rates;
+}
