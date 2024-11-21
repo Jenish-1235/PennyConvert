bcd7eaa1ef353f928a13b89368649fcdb750ae90
Jenish Togadiya <jenishtogadiya549@gmail.com>
2024-11-21 11:52:39 +0530

    progress-currency-model-and-ui-fixed

--- app/src/main/java/com/jenish/pennyconvert/Fragments/CurrencyConvertFragment.java
+++ app/src/main/java/com/jenish/pennyconvert/Fragments/CurrencyConvertFragment.java
@@ -7,8 +7,15 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.LinearLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
 
 import com.jenish.pennyconvert.R;
+
+import java.util.concurrent.Exchanger;
 
 public class CurrencyConvertFragment extends Fragment {
 
@@ -17,6 +24,21 @@ public class CurrencyConvertFragment extends Fragment {
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_currency_convert, container, false);
+
+        EditText amountInput = view.findViewById(R.id.amountInput);
+        Spinner baseCurrencyInput = view.findViewById(R.id.baseCurrencyInput);
+        Spinner targetCurrencyInput = view.findViewById(R.id.targetCurrencyInput);
+        Button convertButton = view.findViewById(R.id.convertButton);
+        TextView convertValueView = view.findViewById(R.id.convertValueView);
+        LinearLayout factsViewLinearLayout = view.findViewById(R.id.factsViewLinearLayout);
+
+        Thread getCurrenciesThread = new Thread(new Runnable(){
+            @Override
+            public void run() {
+
+            }
+        });
+
         return view;
     }
 }
