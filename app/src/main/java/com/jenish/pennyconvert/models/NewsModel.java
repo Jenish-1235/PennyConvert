567864beab5951da7b78697ccf66bffb2e72d517
Jenish Togadiya <jenishtogadiya549@gmail.com>
2024-11-22 03:49:28 +0530

    feature-news-data-model

---
+++ app/src/main/java/com/jenish/pennyconvert/models/NewsModel.java
@@ -0,0 +1,11 @@
+package com.jenish.pennyconvert.models;
+
+public class NewsModel {
+    public String headline;
+    public Long newsId;
+    public String image;
+    public String related;
+    public String source;
+    public String summary;
+    public String url;
+}
